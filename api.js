55ef3625b2ea304c07de70aeade25eac0934ced7
Bazouzouz <bazalbion@gmail.com>
2025-05-05 21:09:54 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,7 @@
+async function fetchPrices(itemIds, city) {
+  const ids = itemIds.join(",");
+  const url = `https://west.albion-online-data.com/api/v2/stats/prices/${ids}?locations=${city}&qualities=1`;
+  const response = await fetch(url);
+  return response.json();
+}
 // Appels à l'API Albion Online
